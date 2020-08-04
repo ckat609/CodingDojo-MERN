558b26e5af4a8ff6a4a2f410d4c591b286286d55
Armando Tello <ckat609@yahoo.com>
2020-08-04 18:14:21 -0500

    finished hook form

---
+++ mern-014-hook-form/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import UserForm from "./components/UserForm";
+
+function App() {
+  return (
+    <div className="container">
+      <UserForm></UserForm>
+    </div>
+  );
+}
+
+export default App;
