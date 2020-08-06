49e8f2de51e1a31dedb5513a4668f4317a709f94
Armando Tello <ckat609@yahoo.com>
2020-08-05 22:16:47 -0500

    basic setup

---
+++ mern-018-shopping-list/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1>HELLO WILL</h1>
+      <h3>believe!!!</h3>
+    </>
+  );
+}
+
+export default App;
